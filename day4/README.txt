2c9747e6a6663e1b5f1d256b39d871b449294b6b
Kaushal Modi <kaushal.modi@gmail.com>
2019-12-06 11:39:23 -0500

    Add day4 part2 solution
    Also update regression suite for the change in password validity
    check.
    
    Part 1: nim c -d:release -d:part1 day4.nim
    Part 2: nim c -d:release day4.nim

--- day4/README.txt
+++ day4/README.txt
@@ -1,3 +1,12 @@
+** Instructions to run part 1 code
+
+    nim c -d:release -d:part1 day4.nim
+
+** Instructions to run part 2 code
+
+    nim c -d:release day4.nim
+
+
 --- Day 4: Secure Container ---
 
 You arrive at the Venus fuel depot only to discover it's protected by
@@ -28,3 +37,22 @@ How many different passwords within the range given in your puzzle
 input meet these criteria?
 
 Your puzzle input is 246515-739105.
+
+--- Part Two ---
+
+An Elf just remembered one more important detail: the two adjacent
+matching digits are not part of a larger group of matching digits.
+
+Given this additional criterion, but still ignoring the range rule, the following are now true:
+
+    112233 meets these criteria because the digits never decrease and
+    all repeated digits are exactly two digits long.
+
+    123444 no longer meets the criteria (the repeated 44 is part of a
+    larger group of 444).
+
+    111122 meets the criteria (even though 1 is repeated more than
+    twice, it still contains a double 22).
+
+How many different passwords within the range given in your puzzle
+input meet all of the criteria?
--- day4/day4.nim
+++ day4/day4.nim
@@ -1,4 +1,7 @@
-import std/[strutils]
+import std/[strutils, strformat, sequtils]
+
+const
+  pswdLength = 6
 
 proc isValidPassword(pswd: int): bool =
   result = false
@@ -6,24 +9,36 @@ proc isValidPassword(pswd: int): bool =
     pswdStr = $pswd
 
   # length check
-  if pswdStr.len != 6:
+  if pswdStr.len != pswdLength:
     return
 
   # digits same or incr
   var
     prevDigit = 0
-    doublesCount = 0
-  for digit in pswdStr:
+    doublesCount: array[pswdLength, int]
+  for idx, digit in pswdStr:
     let
       currentDigit = parseInt($digit)
     if currentDigit < prevDigit:
       return
     if currentDigit == prevDigit:
-      doublesCount.inc
+      doublesCount[idx] = 1
+      when not defined(part1):
+        if idx >= 1 and doublesCount[idx-1] > 0:
+          # If the previous digit was a double count too, increment the
+          # double count counter for both current and previous
+          # indices. That way we can ensure that legal double counts
+          # always have a value of 1.
+          doublesCount[idx].inc
+          doublesCount[idx-1].inc
+    when defined(debug):
+      echo doublesCount
     prevDigit = currentDigit
 
+  when defined(debug):
+    echo ""
   # at least one set of double digits
-  return doublesCount > 0
+  return doublesCount.anyIt(it == 1)
 
 proc numValidPasswords(bounds: Slice[int]): int =
   for p in bounds:
@@ -33,23 +48,47 @@ proc numValidPasswords(bounds: Slice[int]): int =
 when isMainModule:
   import std/[unittest]
 
-  suite "password validity checks":
-    test "length == 6 checks":
+  when defined(part1):
+    suite "part 1 only password validity checks":
+      test &"length == {pswdLength}, digits same or incr, at least one double checks":
+        check:
+          isValidPassword(111111) == true
+
+  suite "part 1 + part 2 password validity checks":
+    test &"length == {pswdLength} checks":
       check:
         isValidPassword(11111) == false
-        isValidPassword(111111) == true
+        isValidPassword(112233) == true
 
     test "digits same or incr":
       check:
-        isValidPassword(111111) == true
+        isValidPassword(111122) == true
+        isValidPassword(112233) == true
         isValidPassword(223450) == false
 
     test "at least one double":
       check:
-        isValidPassword(111111) == true
+        isValidPassword(111122) == true
         isValidPassword(123789) == false
 
-  suite "day4 challenge":
-    test "check":
-      check:
-        numValidPasswords(246515 .. 739105) == 1048
+  when not defined(part1):
+    suite "part 2 only extra password validity check":
+      test "a double is not part of a larger group of matching digits":
+        check:
+          isValidPassword(111111) == false
+          isValidPassword(123444) == false
+          isValidPassword(124445) == false
+          isValidPassword(111123) == false
+          isValidPassword(112233) == true
+          isValidPassword(111122) == true
+
+  when defined(part1):
+    suite "day4 part1 challenge":
+      test "check":
+        check:
+          numValidPasswords(246515 .. 739105) == 1048
+  else:
+    suite "day4 part2 challenge":
+      test "check":
+        check:
+          numValidPasswords(246515 .. 739105) == 677
