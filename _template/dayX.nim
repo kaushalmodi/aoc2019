af2547561eae77636bf203abb2aec03003dfdf77
Kaushal Modi <kaushal.modi@gmail.com>
2019-12-07 20:53:29 -0500

    Add template

---
+++ _template/dayX.nim
@@ -0,0 +1,19 @@
+import std/[strformat]
+import days_utils
+
+when isMainModule:
+  import std/[unittest]
+
+  suite "dayX tests":
+    setup:
+
+    test "":
+      check:
+
+  suite "dayX part1 challenge":
+    test "check":
+      check:
+
+  suite "dayX part2 challenge":
+    test "check":
+      check:
