2877dbceb58c70b2ec7aedafad05417766c80871
Kaushal Modi <kaushal.modi@gmail.com>
2019-12-09 10:45:03 -0500

    Template tweak and fix Org link rendering on Gitlab

--- _template/dayX.nim
+++ _template/dayX.nim
@@ -6,14 +6,18 @@ when isMainModule:
 
   suite "dayX tests":
     setup:
+      discard
 
     test "":
       check:
+        true
 
   suite "dayX part1 challenge":
     test "check":
       check:
+        true
 
   suite "dayX part2 challenge":
     test "check":
       check:
+        true
