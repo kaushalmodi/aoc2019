a29e6d69f8ed615be64a6a10c986ffd392f57c76
Kaushal Modi <kaushal.modi@gmail.com>
2019-12-10 07:30:48 -0500

    Add day10 part2 solution

--- day10/day10.nim
+++ day10/day10.nim
@@ -1,10 +1,13 @@
-import std/[strformat, strutils, tables, math]
+import std/[strutils, tables, math, algorithm]
+when defined(debug):
+  import std/[strformat]
 
 type
   Coord = tuple
     x: int
     y: int
   Map = Table[Coord, int]
+  AngleMap = OrderedTable[float, tuple[coord: Coord, dist: float]]
 
 proc initMap(map: seq[string]): Map =
   for y, line in map:
@@ -43,58 +46,175 @@ proc calcAngle(a, b: Coord): float =
     else: # b is in bottom-left quadrant with respect to a
       return 270.0 - acuteAngleAlongXAxis
 
-proc updateAsteroidsDetCounts(map: var Map; loc: Coord) =
-  var
-    angleMap: Table[float, bool]
+proc calcDistance(a, b: Coord): float =
+  ## Return distance between Cartesian coordinates ``a`` and ``b``.
+  return ((a.x-b.x)^2 + (a.y-b.y)^2).float.sqrt()
+
+proc updateAsteroidsDetCounts(map: var Map; loc: Coord): AngleMap =
   for coord in map.keys:
     if coord == loc:
       continue
-    else:
-      let
-        angle = calcAngle(loc, coord)
-      when defined(debug2):
-        echo &"{coord} at {angle}"
-      angleMap[angle] = true
+    let
+      angle = calcAngle(loc, coord)
+      dist = calcDistance(loc, coord)
+    when defined(debug):
+      echo &"{coord} :: {angle} {dist}"
+    if not result.hasKey(angle) or
+       result.hasKey(angle) and dist < result[angle].dist:
+      result[angle] = (coord, dist)
+
+  # Sort the AngleMap type table by the angle keys.
+  result.sort(cmp)
+
+  when defined(debug3):
+    echo &"From {loc} asteroid:"
+    for angle in result.keys:
+      echo &"  {result[angle].coord} at {angle}"
   let
-    seenAsteroids = angleMap.len
-  when defined(debug):
+    seenAsteroids = result.len
+  when defined(debug3):
     echo &"{seenAsteroids} asteroids seen from {loc}"
   map[loc] = seenAsteroids
+
+proc maxNumAsteroidsDetected(map: var Map): (Coord, int) =
+  for coord in map.keys:
+    discard map.updateAsteroidsDetCounts(coord)
+    if map[coord] > result[1]:
+      result = (coord, map[coord])
 
 proc maxNumAsteroidsDetected(mapStr: seq[string]): (Coord, int) =
   var
     map = mapStr.initMap()
-  for coord in map.keys:
-    map.updateAsteroidsDetCounts(coord)
-    if map[coord] > result[1]:
-      result = (coord, map[coord])
+  return map.maxNumAsteroidsDetected()
+
+proc getVaporizedAsteroids(map: var Map; loc: Coord): seq[Coord] =
+  var
+    mapCopy = map
+  while true:
+    let
+      angleMap = mapCopy.updateAsteroidsDetCounts(loc)
+    mapCopy.del(loc) # Remove the current asteroid from the map
+    for key, val in angleMap.pairs:
+      mapCopy.del(val.coord)
+      when defined(debug):
+        echo &".. Vaporized asteroid {result.len:>3} at {val.coord} @ {key}"
+      result.add(val.coord)
+    if mapCopy.len == 0:
+      break
+
+proc getVaporizedAsteroids(mapStr: seq[string]; loc: Coord): seq[Coord] =
+  var
+    map = mapStr.initMap()
+  return map.getVaporizedAsteroids(loc)
 
 when isMainModule:
   import std/[unittest]
   import days_utils
 
-  suite "day10 tests":
+  suite "day10 small examples":
     test "example 1":
       check:
-        "example1.txt".readFileToStrSeq().maxNumAsteroidsDetected() == ((3, 4).Coord, 8)
+        "example1.txt".readFileToStrSeq().maxNumAsteroidsDetected() == ((3, 4), 8)
 
     test "example 2":
       check:
-        "example2.txt".readFileToStrSeq().maxNumAsteroidsDetected() == ((5, 8).Coord, 33)
+        "example2.txt".readFileToStrSeq().maxNumAsteroidsDetected() == ((5, 8), 33)
 
     test "example 3":
       check:
-        "example3.txt".readFileToStrSeq().maxNumAsteroidsDetected() == ((1, 2).Coord, 35)
+        "example3.txt".readFileToStrSeq().maxNumAsteroidsDetected() == ((1, 2), 35)
 
     test "example 4":
       check:
-        "example4.txt".readFileToStrSeq().maxNumAsteroidsDetected() == ((6, 3).Coord, 41)
+        "example4.txt".readFileToStrSeq().maxNumAsteroidsDetected() == ((6, 3), 41)
+
+  suite "day10 vaporize example":
+    setup:
+      var
+        map = "example6.txt".readFileToStrSeq().initMap()
+      let
+        vaporizedAsteroids = map.getVaporizedAsteroids((8, 3))
+
+    test "example 6":
+      check:
+        vaporizedAsteroids.len == 36
+        #                        1 1 1 1 1 1 1
+        #    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6
+        #  0 . # . . . . # # # 2 4 . . . # . .
+        #  1 # # . . . # # . 1 3 # 6 7 . . 9 #
+        #  2 # # . . . # . . . 5 . 8 # # # # .
+        #  3 . . # . . . . . X . . . # # # . .
+        #  4 . . # . # . . . . . # . . . . # #
+        vaporizedAsteroids[0 .. 8] == @[(8, 1), (9, 0), (9, 1), (10, 0), (9, 2),
+                                        (11, 1), (12, 1), (11, 2), (15, 1)]
+
+        #                        1 1 1 1 1 1 1
+        #    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6
+        #  0 . # . . . . # # # . . . . . # . .
+        #  1 # # . . . # # . . . # . . . . . #
+        #  2 # # . . . # . . . . . . 1 2 3 4 .
+        #  3 . . # . . . . . X . . . 5 # # . .
+        #  4 . . # . 9 . . . . . 8 . . . . 7 6
+        vaporizedAsteroids[9 .. 17] == @[(12, 2), (13, 2), (14, 2), (15, 2), (12, 3),
+                                         (16, 4), (15, 4), (10, 4), (4, 4)]
+
+        #                        1 1 1 1 1 1 1
+        #    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6
+        #  0 . 8 . . . . # # # . . . . . # . .
+        #  1 5 6 . . . 9 # . . . # . . . . . #
+        #  2 3 4 . . . 7 . . . . . . . . . . .
+        #  3 . . 2 . . . . . X . . . . # # . .
+        #  4 . . 1 . . . . . . . . . . . . . .
+        vaporizedAsteroids[18 .. 26] == @[(2, 4), (2, 3), (0, 2), (1, 2), (0, 1),
+                                          (1, 1), (5, 2), (1, 0), (5, 1)]
+
+        #                        1 1 1 1 1 1 1
+        #    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6
+        #  0 . . . . . . 2 3 4 . . . . . 6 . .
+        #  1 . . . . . . 1 . . . 5 . . . . . 7
+        #  2 . . . . . . . . . . . . . . . . .
+        #  3 . . . . . . . . X . . . . 8 9 . .
+        #  4 . . . . . . . . . . . . . . . . .
+        vaporizedAsteroids[27 .. 35] == @[(6, 1), (6, 0), (7, 0), (8, 0), (10, 1),
+                                          (14, 0), (16, 1), (13, 3), (14, 3)]
+
+  suite "day10 big example":
+    setup:
+      const
+        refCoord = (11, 13).Coord
+      var
+        map = "example5.txt".readFileToStrSeq().initMap()
+      let
+        vaporizedAsteroids = map.getVaporizedAsteroids(refCoord)
 
     test "example 5":
       check:
-        "example5.txt".readFileToStrSeq().maxNumAsteroidsDetected() == ((11, 13).Coord, 210)
+        map.maxNumAsteroidsDetected() == (refCoord, 210)
+        vaporizedAsteroids.len == 299
+        vaporizedAsteroids[0] == (11, 12)
+        vaporizedAsteroids[1] == (12, 1)
+        vaporizedAsteroids[2] == (12, 2)
+        vaporizedAsteroids[9] == (12, 8)
+        vaporizedAsteroids[19] == (16, 0)
+        vaporizedAsteroids[49] == (16, 9)
+        vaporizedAsteroids[99] == (10, 16)
+        vaporizedAsteroids[198] == (9, 6)
+        vaporizedAsteroids[199] == (8, 2)
+        vaporizedAsteroids[200] == (10, 9)
+        vaporizedAsteroids[298] == (11, 1)
 
-  suite "day10 part1 challenge":
-    test "check":
+  suite "day10 challenges":
+    setup:
+      var
+        map = "input.txt".readFileToStrSeq().initMap()
+        refVal = map.maxNumAsteroidsDetected()
+      let
+        vaporizedAsteroids = "input.txt".readFileToStrSeq().getVaporizedAsteroids(refVal[0])
+
+    test "part1":
       check:
-        "input.txt".readFileToStrSeq().maxNumAsteroidsDetected() == ((19, 14).Coord, 274)
+        refVal[1] == 274
+
+    test "part2":
+      check:
+        vaporizedAsteroids[199].x * 100 + vaporizedAsteroids[199].y == 305
