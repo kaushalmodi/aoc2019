0dd3f97d6c2b0a76752857761a6ab2531803a46c
Kaushal Modi <kaushal.modi@gmail.com>
2019-12-10 11:05:22 -0500

    day10: TIL that arctan2 exists :)
    To run using my earlier manual implementation of arctan2:
    
        nim c -d:release -d:usingarctan -r day10
    
    To run using the math.arctan2:
    
        nim c -d:release -r day10
    
    Refactor: angle -> slope

--- day10/day10.nim
+++ day10/day10.nim
@@ -7,7 +7,7 @@ type
     x: int
     y: int
   Map = Table[Coord, int]
-  AngleMap = OrderedTable[float, tuple[coord: Coord, dist: float]]
+  SlopeMap = OrderedTable[float, tuple[coord: Coord, dist: float]]
 
 proc initMap(map: seq[string]): Map =
   for y, line in map:
@@ -15,61 +15,84 @@ proc initMap(map: seq[string]): Map =
       if ch == '#':
         result[(x, y).Coord] = 0
 
-proc calcAngle(a, b: Coord): float =
-  ## Return the angle of ``a`` looking at ``b`` in degrees.
-  ## Angle is 0 degrees when ``b`` is in a straight line above ``a``.
-  ## Angle is 90 degrees when ``b`` is in a straight line to the right of ``a``.
-  ## Angle is 180 degrees when ``b`` is in a straight line below ``a``.
-  ## And so on ..
-  doAssert a != b
-  if a.x == b.x:
-    if a.y > b.y:
-      return 0.0 # b is in a straight line above a
-    else:
-      return 180.0 # b is in a straight line below a
-  if a.y == b.y:
+when not defined(usingArctan):
+  proc calcSlope(a, b: Coord; flipX = false, flipY = false): float =
+    ## Return the slope of ``a`` looking at ``b`` in degrees.
+    ## Slope is 0 degrees when ``b`` is in a straight line above ``a``.
+    ## Slope is 90 degrees when ``b`` is in a straight line to the right of ``a``.
+    ## Slope is 180 degrees when ``b`` is in a straight line below ``a``.
+    ## And so on ..
+    doAssert a != b
+    let
+      yDelta = (b.y - a.y).float * pow(-1, float(flipY))
+      xDelta = (b.x - a.x).float * pow(-1, float(flipX))
+    result = arctan2(xDelta, yDelta).radToDeg()
+    if result < 0:
+      result = result + 360
+else:                       # The flipX and flipY params below are just for
+                            # compatibility with the above proc's signature.
+  proc calcSlope(a, b: Coord; flipX = false, flipY = false): float =
+    ## Return the slope of ``a`` looking at ``b`` in degrees.
+    ## Slope is 0 degrees when ``b`` is in a straight line above ``a``.
+    ## Slope is 90 degrees when ``b`` is in a straight line to the right of ``a``.
+    ## Slope is 180 degrees when ``b`` is in a straight line below ``a``.
+    ## And so on ..
+    doAssert a != b
+    if a.x == b.x:
+      if a.y > b.y:
+        return 0.0 # b is in a straight line above a
+      else:
+        return 180.0 # b is in a straight line below a
+    if a.y == b.y:
+      if a.x < b.x:
+        return 90.0 # b is in a straight line to the right of a
+      else:
+        return 90.0 + 180.0 # b is in a straight line to the left of a
+    let
+      oppAdj = abs(a.y - b.y) / abs(a.x - b.x)
+      acuteAngleAlongXAxis = oppAdj.arctan().radToDeg()
     if a.x < b.x:
-      return 90.0 # b is in a straight line to the right of a
+      if b.y < a.y: # b is in top-right quadrant with respect to a
+        return 90.0 - acuteAngleAlongXAxis
+      else: # b is in bottom-right quadrant with respect to a
+        return 90.0 + acuteAngleAlongXAxis
     else:
-      return 90.0 + 180.0 # b is in a straight line to the left of a
-  let
-    oppAdj = abs(a.y - b.y) / abs(a.x - b.x)
-    acuteAngleAlongXAxis = oppAdj.arctan().radToDeg()
-  if a.x < b.x:
-    if b.y < a.y: # b is in top-right quadrant with respect to a
-      return 90.0 - acuteAngleAlongXAxis
-    else: # b is in bottom-right quadrant with respect to a
-      return 90.0 + acuteAngleAlongXAxis
-  else:
-    if b.y < a.y: # b is in top-left quadrant with respect to a
-      return 270.0 + acuteAngleAlongXAxis
-    else: # b is in bottom-left quadrant with respect to a
-      return 270.0 - acuteAngleAlongXAxis
+      if b.y < a.y: # b is in top-left quadrant with respect to a
+        return 270.0 + acuteAngleAlongXAxis
+      else: # b is in bottom-left quadrant with respect to a
+        return 270.0 - acuteAngleAlongXAxis
 
 proc calcDistance(a, b: Coord): float =
   ## Return distance between Cartesian coordinates ``a`` and ``b``.
   return ((a.x-b.x)^2 + (a.y-b.y)^2).float.sqrt()
 
-proc updateAsteroidsDetCounts(map: var Map; loc: Coord): AngleMap =
+proc updateAsteroidsDetCounts(map: var Map; loc: Coord): SlopeMap =
   for coord in map.keys:
     if coord == loc:
       continue
     let
-      angle = calcAngle(loc, coord)
+      # From the puzzle description:
+      #   The asteroids can be described with X,Y coordinates where X is
+      #   the distance from the left edge and Y is the distance from the
+      #   top edge (so the top-left corner is 0,0 and the position
+      #   immediately to its right is 1,0).
+      # So the Y axis is actually flipped! .. numbers are incrementing
+      # while going downwards on Y axis.
+      slope = calcSlope(loc, coord, flipY = true)
       dist = calcDistance(loc, coord)
     when defined(debug):
-      echo &"{coord} :: {angle} {dist}"
-    if not result.hasKey(angle) or
-       result.hasKey(angle) and dist < result[angle].dist:
-      result[angle] = (coord, dist)
+      echo &"{coord} :: {slope} {dist}"
+    if not result.hasKey(slope) or
+       result.hasKey(slope) and dist < result[slope].dist:
+      result[slope] = (coord, dist)
 
-  # Sort the AngleMap type table by the angle keys.
+  # Sort the SlopeMap type table by the slope keys.
   result.sort(cmp)
 
   when defined(debug3):
     echo &"From {loc} asteroid:"
-    for angle in result.keys:
-      echo &"  {result[angle].coord} at {angle}"
+    for slope in result.keys:
+      echo &"  {result[slope].coord} at {slope}"
   let
     seenAsteroids = result.len
   when defined(debug3):
@@ -87,9 +110,9 @@ proc getVaporizedAsteroids(map: var Map; loc: Coord): seq[Coord] =
     mapCopy = map
   while true:
     let
-      angleMap = mapCopy.updateAsteroidsDetCounts(loc)
+      slopeMap = mapCopy.updateAsteroidsDetCounts(loc)
     mapCopy.del(loc) # Remove the current asteroid from the map
-    for key, val in angleMap.pairs:
+    for key, val in slopeMap.pairs:
       mapCopy.del(val.coord)
       when defined(debug):
         echo &".. Vaporized asteroid {result.len:>3} at {val.coord} @ {key}"
