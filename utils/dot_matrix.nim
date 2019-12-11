b02b880e23158a830fa98fef4529f8b28dec8955
Kaushal Modi <kaushal.modi@gmail.com>
2019-12-11 16:19:48 -0500

    dot_matrix: Minor refactoring
    - Make parseLetter return a char
    - Add more tests

--- utils/dot_matrix.nim
+++ utils/dot_matrix.nim
@@ -115,7 +115,14 @@ const
              "   # ",
              "  #  ",
              " #...",
-             " ####"]
+             " ####"],
+
+      " ": [".....",
+            ".....",
+            ".....",
+            ".....",
+            ".....",
+            "....."]
     }.toTable
 
 var
@@ -134,28 +141,24 @@ proc transpose*[T](bits: seq[T]; letterWidth = dot5x6Width, letterHeight = dot5x
     for letterId in 0 ..< bitsPerRow div letterWidth:
       result[letterId].add(rowBits[letterId*letterWidth ..< (letterId+1)*letterWidth])
 
-proc parseLetter*[T](letter: openArray[T]; letterWidth = dot5x6Width, letterHeight = dot5x6Height): string =
+proc parseLetter*[T](dots: openArray[T]; letterWidth = dot5x6Width, letterHeight = dot5x6Height): char =
   # Support only 5x6 dot matrix for now.
   doAssert letterWidth == dot5x6Width
   doAssert letterHeight == dot5x6Height
-  var
-    matchedLetter: char
+  doAssert dots.len == dot5x6Width*dot5x6Height
   for l, pat in patterns5x6Flat.pairs:
-    matchedLetter = l[0]
-    for i in 0 ..< letterWidth*letterHeight:
-      if pat[i] == '#' and letter[i] != 1:
-        matchedLetter = '\0'
+    result = l[0]
+    for i, dot in dots:
+      if pat[i] == '#' and dot == 0 or
+         pat[i] == '.' and dot != 0:
+        # Start looking at the next pattern if the input dots seq has
+        # a 0 where a 1 was expected, or vice-versa.
+        result.reset()
         break
-      if pat[i] == '.' and letter[i] != 0:
-        matchedLetter = '\0'
-        break
-    if matchedLetter != '\0':
-      break
-  if matchedLetter == '\0':
-    echo &"[Error] Letter parsing failed for {letter}"
-    result = ""
-  else:
-    result = $matchedLetter
+    # Stop searching patterns once a match is found.
+    if result.ord > 0: break
+  if result.ord == 0:
+    echo &"[Error] Letter parsing failed for {dots}"
 
 proc parseLetters*[T](letters: openArray[seq[T]]; letterWidth = dot5x6Width, letterHeight = dot5x6Height): string =
   for letter in letters:
@@ -184,43 +187,51 @@ when isMainModule:
 
     # test "B":
     #   check:
-    #     @[].parseLetter() == "B"
+    #     @[].parseLetter() == 'B'
+
+    test "failed match check":
+      check:
+        @[2, 5, 2, 1, 6, 4, 3, 7, 0, 8, 2, 8, 3, 5, 7, 7, 4, 0, 2, 5, 3, 3, 1, 8, 1, 0, 8, 0, 4, 8].parseLetter() == '\0'
+
+    test "space":
+      check:
+        @[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].parseLetter() == ' '
 
     test "A":
       check:
-        @[0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0].parseLetter() == "A"
+        @[0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0].parseLetter() == 'A'
 
     test "B":
       check:
-        @[0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0].parseLetter() == "B"
+        @[0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0].parseLetter() == 'B'
 
     test "E":
       check:
-        @[0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1].parseLetter() == "E"
-        @[1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0].parseLetter() == "E"
+        @[0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1].parseLetter() == 'E'
+        @[1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0].parseLetter() == 'E'
 
     test "G":
       check:
-        @[0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1].parseLetter() == "G"
-        @[0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0].parseLetter() == "G"
+        @[0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1].parseLetter() == 'G'
+        @[0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0].parseLetter() == 'G'
 
     test "J":
       check:
-        @[0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0].parseLetter() == "J"
-        @[0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0].parseLetter() == "J"
+        @[0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0].parseLetter() == 'J'
+        @[0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0].parseLetter() == 'J'
 
     test "L":
       check:
-        @[0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1].parseLetter() == "L"
+        @[0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1].parseLetter() == 'L'
 
     test "P":
       check:
-        @[0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0].parseLetter() == "P"
-        @[0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0].parseLetter() == "P"
+        @[0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0].parseLetter() == 'P'
+        @[0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0].parseLetter() == 'P'
 
     test "Z":
       check:
-        @[0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1].parseLetter() == "Z"
+        @[0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1].parseLetter() == 'Z'
 
   suite "letter seq":
 
